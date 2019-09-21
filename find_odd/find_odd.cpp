827d201aca02fced9f34b822a811177e5f99c4b6
dave <ddalcino@horizon.csueastbay.edu>
2019-09-21 15:17:19 -0700

    Add google tests and findOddBest17

--- find_odd/find_odd.cpp
+++ find_odd/find_odd.cpp
@@ -7,6 +7,9 @@
 #include <iostream>
 #include <numeric>
 #include <vector>
+#include <execution>
+#include <benchmark/benchmark.h>
+#include <memory>
 
 using std::accumulate;
 using std::bit_xor;
@@ -17,7 +20,7 @@ using std::vector;
 // Bad implementation of findOdd
 // Compares every element against every other element
 // Earliest it can exit is after one full scan of the vector
-int findOddBad(vector<int> v) {
+int findOddBad(const vector<int> &v) {
   // For each element in the vector
   for (auto i : v) {
     // Compare against all other vector elements
@@ -51,7 +54,7 @@ int findOddBad(vector<int> v) {
 //  Performing this operations can occur in any order because of (1),
 //  so we can just intuitively XOR from the beginning to the end.
 // Much better than the previous, but still a little "old-fashioned"
-int findOddBetter(vector<int> v) {
+int findOddBetter(const vector<int> &v) {
   // Accumulate partial results in tmp
   int tmp = 0;
 
@@ -66,24 +69,60 @@ int findOddBetter(vector<int> v) {
 // How about 1 line of code!
 // Performs the same operation as the above, but we're making use of
 // the STL!
-int findOddBest(vector<int> v) {
+int findOddBest(const vector<int> &v) {
   // Bit-wise XOR of all elements
-  return accumulate(begin(v), end(v), 0, bit_xor<int>());
+  return accumulate(begin(v), end(v), 0, bit_xor<>());
 }
 
-int main() {
-  // Create a vector
-  vector<int> v = {2, 2, 3, 4, 4, 1, 1, 3, 4};
-
-  // Get 4 solutions using our 4 functions
-  int sol1 = findOddBad(v);
-  int sol2 = findOddBetter(v);
-  int sol3 = findOddBest(v);
-
-  // Print out the results
-  cout << "sol1 == " << sol1 << endl;
-  cout << "sol2 == " << sol2 << endl;
-  cout << "sol3 == " << sol3 << endl;
-
-  return 0;
+int findOddBest17(const vector<int> &v) {
+  // Bit-wise XOR of all elements
+  return reduce(std::execution::par, begin(v), end(v), 0, bit_xor<>());
 }
+
+// Create a vector
+static const vector<int> makeTestVec() {
+  vector<int> v = vector<int>(static_cast<size_t>(1'000'000ull), 5);
+  v.emplace_back(1);
+  return v;
+}
+static const vector<int> &testVector() {
+  static const vector<int> v{makeTestVec()};
+  return v;
+}
+
+// Helper function that prevents elision of the whole test during a release build
+void dont_elide_me(bool b) {
+  if (!b)
+    exit(1);
+}
+
+static void BM_findOddBad(benchmark::State &state) {
+  for (auto _ : state)
+    dont_elide_me(findOddBad(testVector()));
+}
+// Register the function as a benchmark
+BENCHMARK(BM_findOddBad);
+
+static void BM_findOddBetter(benchmark::State &state) {
+  for (auto _ : state)
+    dont_elide_me(findOddBetter(testVector()));
+}
+// Register the function as a benchmark
+BENCHMARK(BM_findOddBetter);
+
+static void BM_findOddBest(benchmark::State &state) {
+  for (auto _ : state)
+    dont_elide_me(findOddBest(testVector()));
+}
+// Register the function as a benchmark
+BENCHMARK(BM_findOddBest);
+
+static void BM_findOddBest17(benchmark::State &state) {
+  for (auto _ : state)
+    dont_elide_me(findOddBest17(testVector()));
+}
+// Register the function as a benchmark
+BENCHMARK(BM_findOddBest17);
+
+BENCHMARK_MAIN();
+
